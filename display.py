91c41c3c2bd09bf99a188d4e68358db21bdffc76
brettrd <brettrd@brettrd.com>
2018-01-24 21:23:58 +0000

    allocate correct number of LEDs

--- display.py
+++ display.py
@@ -5,9 +5,11 @@ from time import sleep
 from fire import Fire
 from neopixel import *
 
+cols=24
+rows=45
 
 # LED strip configuration:
-LED_COUNT      = 40      # Number of LED pixels.
+LED_COUNT      = rows*cols      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
@@ -15,10 +17,6 @@ LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0
 LED_STRIP      = ws.SK6812_STRIP_GRBW
-
-
-cols=4
-rows=4
 
 sim = Fire(cols,rows)
 
